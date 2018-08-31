cab506edde7cd023c26a9025f925616401ce6f4e
Brent Seidel <brentseidel@mac.com>
2018-08-31 15:41:27 -0700

    Minor updates to setting.html and test1.js
    Update the settings page to remove some test stuff and add buttons and code
    to send commands to the network controller.

--- Web/test1.js
+++ Web/test1.js
@@ -1,30 +1,4 @@
 //
-// Simple function to put some output into the "demo" element.
-//
-function test()
-{
-  var text = "";
-  var x = 0;
-
-  for (x = 0; x < 10; x++)
-  {
-    text += "Test " + x + "<br>";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
-//
-// Get numbers from two fields, add them together and put the result in another
-// field and the "demo" element.
-//
-function calculate()
-{
-  var num1 = document.getElementById("num1").value * 1;
-  var num2 = document.getElementById("num2").value * 1;
-
-  document.getElementById("num3").value = num1 + num2;
-  document.getElementById("demo").innerHTML = "Calculating " + (num1 + num2);
-}
-//
 // Requests a value from the server using AJAX.
 //
 function loadCounter()
@@ -52,7 +26,22 @@ function displayCounter(xml)
                                                tasks + " current active tasks";
 }
 //
-// AJAX call to send a command
+// AJAX call to send a command passed as a string
+//
+function sendCommand(cmd)
+{
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      cmdResp(this);
+    }
+  };
+  xhttp.open("GET", "/xml/Command?command=" + cmd, true);
+  xhttp.send();
+}
+//
+// AJAX call to send a command contained in a field
 //
 function sendCmd()
 {
@@ -67,6 +56,8 @@ function sendCmd()
   xhttp.open("GET", "/xml/Command?command=" + cmd, true);
   xhttp.send();
 }
+//
+// AJAX response to display the command response.
 function cmdResp(xml)
 {
   var xmlDoc = xml.responseXML;
@@ -185,79 +176,5 @@ function dispDevData(xml)
   }
     document.getElementById("DevData").innerHTML = text;
 }
-//
-// Global values for some timer related functions.
-//
-var therm_value;
-var therm_timer;
-//
-// Initializes the value and starts a timer to count up.  Apparently this can't
-// just be called "animate".  Perhaps some browsers define it and some don't.
-//
-function animateThings()
-{
 
-  alert("Animate called.");
-  therm_value = 0;
-  therm_timer = window.setInterval(update_therm_up, 250);
-}
-//
-// Counts up and updates a couple images on each count.
-//
-function update_therm_up()
-{
-  var el1 = document.getElementById("temp1");
-  var el2 = document.getElementById("temp2");
-  var el3 = document.getElementById("dial1");
-  el1.src = "/Thermometer?min=100&max=350&value=" + (350 - therm_value);
-  el2.src = "/Thermometer?min=0&max=250&value=" + therm_value;
-  el3.src = "/Dial?min=100&max=200&value=" + therm_value;
-  therm_value++;
-  if (therm_value > 250)
-  {
-    window.clearInterval(therm_timer);
-  therm_timer = window.setInterval(update_therm_down, 250);
-  }
-}
-//
-// Counts down and updates a couple images on each count.
-//
-function update_therm_down()
-{
-  var el1 = document.getElementById("temp1");
-  var el2 = document.getElementById("temp2");
-  var el3 = document.getElementById("dial1");
-  el1.src = "/Thermometer?min=100&max=350&value=" + (350 - therm_value);
-  el2.src = "/Thermometer?min=0&max=250&value=" + therm_value;
-  el3.src = "/Dial?min=100&max=200&value=" + therm_value;
-  therm_value--;
-  if (therm_value < 0)
-  {
-    window.clearInterval(therm_timer);
-  }
-}
-//
-// Set the value of thermometer 1
-//
-function setT1()
-{
-  var el = document.getElementById("temp1");
-  el.src = "/Thermometer?min=100&max=350&value=" + document.getElementById("t1").value;
-}
-//
-// Set the value of thermometer 2
-//
-function setT2()
-{
-  var el = document.getElementById("temp2");
-  el.src = "/Thermometer?min=0&max=250&value=" + document.getElementById("t2").value;
-}
-//
-// Set the value of dial 1
-//
-function setD1()
-{
-  var el = document.getElementById("dial1");
-  el.src = "/Dial?min=100&max=200&value=" + document.getElementById("d1").value;
-}
 
